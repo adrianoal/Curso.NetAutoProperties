c68b6488d264bb3c9bf84ae6de6d2d253b361550
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-09 15:18:44 -0300

    Exemplo de Auto Properties

---
+++ AutoProperties/Produtos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+
+namespace AutoProperties
+{
+    class Produto
+    {
+        private string _nome; // Aqui nao pode usar propriedade auto implementada, pq tem uma logica no metodo abaixo: validacao com "if"
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+
+            }
+        }
+
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        //Construtor com 2 parametros
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 0;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+
+        // P/ fazer uma sobreposicao, tem q escrever a palavra "override"
+        public override string ToString()
+        {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
